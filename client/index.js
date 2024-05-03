896a633b3e05d760684da908852795843d4337d2
treebearjason <jasonwct@hotmail.com>
2024-05-03 12:58:31 +0100

    Initial commit

--- client/index.js
+++ client/index.js
@@ -6,12 +6,18 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const searchName = Math.random() > 0.5 ? niceList[Math.floor(Math.random() * niceList.length)] : 'User not in list'
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === searchName)
+  const proof = merkleTree.getProof(index)
 
+  console.log(proof)
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof, searchName
   });
 
-  console.log({ gift });
+  console.log({ searchName, gift });
 }
 
 main();
